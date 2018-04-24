99b7def3ea2b30b0eaf9f16a5c35d110a57b031b
Alain DEMENET <ademenet@e2r9p9.42.fr>
2018-04-24 17:19:20 +0200

    Working script to get EIP offset

--- string_generator/string_generator.py
+++ string_generator/string_generator.py
@@ -7,12 +7,20 @@ import itertools
 UPPER = list(string.ascii_uppercase)
 LOWER = list(string.ascii_lowercase)
 DIGIT = list(string.octdigits)
+# Generate the string "Aa1Aa2Aa3..."
+STRING = str()
+for combination in map("".join, itertools.product(UPPER, LOWER, DIGIT)):
+    STRING = STRING + combination
+
+def _get_offset(offset_str):
+    if '0x' in offset_str:
+        offset = bytes.fromhex(offset_str[2:]).decode('utf-8')
+    else:
+        offset = bytes.fromhex(offset_str).decode('utf-8')
+    return STRING.index(offset)
 
 def _string_generator(size):
-    string = str()
-    for combination in map("".join, itertools.product(UPPER, LOWER, DIGIT)):
-        string = string + combination
-    return string[:size]
+    return STRING[:size]
 
 def _argparser():
     parser = argparse.ArgumentParser(description='This software gives you the EIP offset')
@@ -21,10 +29,12 @@ def _argparser():
     return args
 
 def main():
-    # Parse arguments
     args = _argparser()
-    # String generation
+    print('\033[95m#1 Copy/paste this string in order to get a segfault:\033[0m')
     print(_string_generator(args['size']))
+    print('\033[95m#2 Paste the symbols where segfault occured:\033[0m')
+    offset = input('0x????????: ')
+    print('\033[95m#3 Your EIP offset is: \033[0m', _get_offset(offset))
 
 if __name__ == '__main__':
     main()
